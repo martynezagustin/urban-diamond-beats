6e69aadaf8a82799342f88ac3bd1555b929f94c2
Agustín Martínez Medina <martynezagustin@gmail.com>
2023-08-28 15:03:03 -0300

    mi first commit

---
+++ js/app.js
@@ -0,0 +1,154 @@
+const beatContainer = document.getElementById("beats-container")
+const dropdownMenu = document.querySelector(".dropdown-menu")
+const loadingSpan = document.querySelector(".loading-span")
+let beats = []
+let beatsCart = JSON.parse(localStorage.getItem('beatsCart')) || []
+
+function executeLoading() {
+    loadingSpan.style.color = "deeppink"
+    setInterval(() => {
+        if (loadingSpan.style.color == "deeppink") {
+            loadingSpan.style.color = "whitesmoke"
+        } else {
+            loadingSpan.style.color = "deeppink"
+        }
+    }, 500);
+}
+
+function generateBeats() {
+    clearContent()
+    setTimeout(() => {
+        getApi("../json/beats.json")
+            .then(data => data.json())
+            .then(data => {
+                beats = data
+                generateCategories(beats)
+                generateAllBeats()
+                showAllCategories()
+                setTimeout(() => {
+                    Toastify({
+                        text: "To add beats to cart, click on the respective 'add to cart' buttons.",
+                        duration: 3000,
+                        style: {
+                            background: "blueviolet",
+                            color: "lightpink"
+                        },
+                        close: true
+                        }).showToast();
+                }, 3000);
+            })
+            .catch(() => {
+                const divResult = document.querySelector(".divResult")
+                const errorTitle = document.createElement("h2")
+                errorTitle.innerText = "Ha ocurrido un error, vuelve a intentarlo más tarde."
+                errorTitle.style.color = "crimson"
+                divResult.append(errorTitle)
+                loadingSpan.style.display = 'none'
+            })
+    }, 6000);
+}
+
+function showAllCategories() {
+    const li = document.createElement("li")
+    const btnGenerateAllCategories = document.createElement("a")
+    btnGenerateAllCategories.classList.add("dropdown-item")
+    btnGenerateAllCategories.innerText = "Show all categories"
+    li.appendChild(btnGenerateAllCategories)
+    dropdownMenu.appendChild(li)
+    btnGenerateAllCategories.addEventListener("click", () => {
+        generateAllBeats()
+    })
+}
+
+function generateAllBeats() {
+    clearContent()
+    beats.forEach((beat) => {
+        let options = "" //variable con string vacio
+        for (const licenseType in beat.price) {
+            options += `<option value="${licenseType}">${licenseType}: <strong>U$D</strong> ${beat.price[licenseType]}</option>`
+        }
+        const divisor = document.createElement("div")
+        divisor.classList.add("beat-container__div")
+        divisor.innerHTML = `<img src=${beat.img} alt=${beat.name} class="beat-container__div-img">
+<div>
+<h3 class="beat-container__div-h3">${beat.name}</h3>
+<h4 class = "beat-container__div-h4">${beat.category}</h4>
+<h4>Elegir licencia: </h4>
+<div">
+<select>${options}</select>
+<button class="beat-container__add-to-cart" id=${beat.id}>ADD TO CART 🛒</button>
+</div>
+</div>
+`
+beatContainer.appendChild(divisor)
+loadingSpan.style.display = 'none'
+        const btnAdd = document.querySelectorAll(".beat-container__add-to-cart")
+        btnAdd.forEach((btn) => {
+            btn.addEventListener("click", addToCart)
+        })
+    })
+}
+
+function generateCategories(beats) {
+    dropdownMenu.innerHTML = ""
+    //generate categories
+    const categories = beats.map((beat) => beat.category)
+    const categoriesData = [...new Set(categories)]
+    console.log(categoriesData)
+    categoriesData.forEach((category) => {
+        const li = document.createElement("li")
+        const anchor = document.createElement("a")
+        anchor.classList.add("dropdown-item")
+        anchor.innerText = category
+        li.appendChild(anchor)
+        dropdownMenu.appendChild(li)
+        anchor.addEventListener("click", () => {
+            filterByCategories(category, beats)
+        })
+    })
+}
+
+function filterByCategories(category, beats) {
+    clearContent()
+    const categoryFilter = beats.filter((beat) => beat.category === category)
+    if (categoryFilter.length > 0) {
+        categoryFilter.forEach((beat) => {
+            let options = "" //variable con string vacio
+            for (const licenseType in beat.price) {
+                options += `<option value="${licenseType}">${licenseType}: <strong>U$D</strong> ${beat.price[licenseType]}</option>`
+            }
+            const divisor = document.createElement("div")
+            divisor.classList.add("beat-container__div")
+            divisor.innerHTML = `<img src=${beat.img} alt=${beat.name} class="beat-container__div-img">
+            <div>
+            <h3 class="beat-container__div-h3">${beat.name}</h3>
+            <h4 class = "beat-container__div-h4">${beat.category}</h4>
+            <h4>Elegir licencia: </h4>
+            <div">
+            <select>${options}</select>
+            <button class="beat-container__add-to-cart" id=${beat.id}>ADD TO CART 🛒</button>
+            </div>
+            </div>
+            `
+            beatContainer.appendChild(divisor)
+            loadingSpan.style.display = 'none'
+            const btnAdd = document.querySelectorAll(".beat-container__add-to-cart")
+            btnAdd.forEach((btn) => {
+                btn.addEventListener("click", addToCart)
+            })
+        })
+    }
+}
+
+function getApi(url) {
+    return fetch(url)
+}
+
+function clearContent() {
+    while (beatContainer.firstChild) {
+        beatContainer.removeChild(beatContainer.firstChild)
+    }
+}
+
+generateBeats()
+executeLoading()
---
+++ js/cartFunctions.js
@@ -0,0 +1,132 @@
+const span = document.createElement("span")
+
+function addToCart(e) {
+    const selectLicense = e.target.parentNode.querySelector("select")
+    const beat = beats.find(beat => beat.id === parseInt(e.target.id))
+    const beatExist = beatsCart.find(beat => beat.id === parseInt(e.target.id))
+    let optionsSelected = []
+    if (beatExist) {
+        Toastify({
+            text: "Quantity increased of " + beatExist.name,
+            duration: 2000,
+            style: {
+                background: "whitesmoke",
+                color: "deeppink"
+            }
+        }).showToast()
+        beatExist.quantity
+        beatExist.quantity++
+        beatExist.optionsSelected.push(selectLicense.value)
+        console.log(beatExist.optionsSelected);
+        updateCartBody()
+    } else {
+        const priceLicense = beat.price[selectLicense.value]
+        optionsSelected.push(selectLicense.value)
+        beatsCart.push({
+            ...beat,
+            selectedLicense: selectLicense.value,
+            quantity: 1,
+            priceLicense: priceLicense,
+            optionsSelected: optionsSelected
+        })
+        Toastify({
+            text: "Beat added to cart! " + beat.name,
+            duration: 3000,
+            style: {
+                background: "blueviolet"
+            },
+            close: true
+        }).showToast();
+        updateCartBody()
+    }
+    renderCart()
+    localStorage.setItem('beatsCart', JSON.stringify(beatsCart))
+}
+
+function renderCart() {
+    if (beatsCart.length > 0) {
+        span.innerText = beatsCart.length || ''
+        createSpanCart()
+    }
+}
+
+function createSpanCart() {
+    const cartDiv = document.querySelector("#cartDiv")
+    span.classList.add("span-cart")
+    cartDiv.append(span)
+
+}
+
+function updateCartBody() {
+    const offCanvasBody = document.querySelector("#cart-body")
+    offCanvasBody.innerHTML = ""
+    if (beatsCart.length === 0) {
+        offCanvasBody.innerHTML = `<p>Comienza a añadir beats para sumar a tu carrito!</p>`
+    } else {
+
+        beatsCart.forEach(element => {
+            const div = document.createElement("div")
+            let options = ""
+            let totalPrices = 0
+            for (const license of element.optionsSelected) {
+                options += `<option value="${license}">${license}</option>`
+                totalPrices += element.price[license]
+            }
+            div.classList.add("card-cart")
+            div.classList.add("d-flex")
+            div.classList.add("flex-column")
+            div.innerHTML = `
+            <img src=${element.img} class="card-cart__img mw-100">
+            <h2 class="card-cart__h2">${element.name}</h2>
+            <h3 class="card-cart__h3">${element.category}</h3>
+            <h3 class="card-cart__h3"><strong>Cantidad:</strong> ${element.quantity}</h3>
+            <select>${options}</select>
+            <p>TOTAL DE BEAT: U$D${totalPrices}</p>
+            <button class="card-cart__btn-delete" data-id=${element.id}>DELETE BEAT</button>
+            `
+            offCanvasBody.append(div)
+            const btnDelete = document.querySelectorAll(".card-cart__btn-delete")
+            btnDelete.forEach((btn) => {
+                btn.addEventListener("click", removeBeat)
+            })
+        });
+        const btnEmpty = document.createElement("button")
+        btnEmpty.innerText = "EMPTY CART"
+        btnEmpty.classList.add("btn-empty-cart")
+        offCanvasBody.appendChild(btnEmpty)
+        btnEmpty.addEventListener("click", emptyCart)
+    }
+}
+
+
+function removeBeat(e) {
+    const beatDelete = beatsCart.find(beat => beat.id === parseInt(e.target.dataset.id))
+    const index = beatsCart.indexOf(beatDelete)
+    console.log(beatDelete);
+    if (beatDelete) {
+        const confirmar = confirm("Desea eliminar: " + beatDelete.name + "?")
+        if (confirmar) {
+            alert("Eliminado con exito.")
+            beatsCart.splice(index, 1)
+            localStorage.setItem('beatsCart', JSON.stringify(beatsCart))
+            updateCartBody()
+            renderCart()
+        }
+    }
+}
+
+function emptyCart() {
+    if (beatsCart.length > 0) {
+        const confirmar = confirm("Desea vaciar el carrito?")
+        if (confirmar) {
+            alert("El carrito fue vaciado.")
+            beatsCart.splice(0, beatsCart.length)
+            localStorage.setItem('beatsCart', JSON.stringify(beatsCart))
+            updateCartBody()
+            renderCart()
+        }
+    }
+}
+
+updateCartBody()
+renderCart()
